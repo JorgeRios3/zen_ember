4c6e75c88cc2c27a84319872bbc91dfdc4240577
Jorge Rios <jrios@grupoiclar.com>
2016-03-17 16:46:17 -0600

    depuraciones

--- app/components/link-menu.js
+++ app/components/link-menu.js
@@ -1,13 +1,12 @@
 import Ember from 'ember';
 
 const {
-  computed
-}= Ember;
- var info=Ember.Logger.info;
+  computed,
+  Logger: { info }
+} = Ember;
 export default Ember.Component.extend({
-	opcion:null,
-	consulta: false,
-	isOferta: computed.equal("opcion", "oferta"),
-	isCliente: computed.equal("opcion", "cliente")
-
+  opcion: null,
+  consulta: false,
+  isOferta: computed.equal('opcion', 'oferta'),
+  isCliente: computed.equal('opcion', 'cliente')
 });
