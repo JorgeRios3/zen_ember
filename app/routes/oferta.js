c6c0dcd2f559e818da600447e5ab435b234e1b0d
Domingo <domingo.aguilera@gmail.com>
2016-04-05 12:27:46 -0500

    depuracion jscs de oferta ruta y oferta controller

--- app/routes/oferta.js
+++ app/routes/oferta.js
@@ -1,76 +1,70 @@
 import Ember from 'ember';
-import RouteAuthMixin from "../mixins/routeauth";
-import AuthenticatedRouteMixin from "ember-simple-auth/mixins/authenticated-route-mixin";
-const info = Ember.Logger.info;
+import RouteAuthMixin from '../mixins/routeauth';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 const {
   get,
   set,
-  computed,
-  observer,
-  isEmpty
-}= Ember;
+  isEmpty,
+  Logger: { info },
+  Route,
+  RSVP: { hash }
+} = Ember;
 
-export default Ember.Route.extend(AuthenticatedRouteMixin,
-RouteAuthMixin , 
+export default Route.extend(AuthenticatedRouteMixin, RouteAuthMixin,
 {
-
-
-  cliente: "",
-  noModel:false,  
-
-  beforeModel(transition){
+  cliente: '',
+  noModel: false,
+  beforeModel(transition) {
     this._super(...arguments);
-    
-    set(this, "cliente", "");
-    set(this, "noModel", false);
-    var controller = this.controllerFor(this.routeName);
-    var url = get(this,"router.url");
-    var origenCliente = false;
-    try{
+    set(this, 'cliente', '');
+    set(this, 'noModel', false);
+    let controller = this.controllerFor(this.routeName);
+    let url = get(this, 'router.url');
+    let origenCliente = false;
+    try {
       origenCliente = !isEmpty(transition.queryParams.origenCliente);
-      if(!isEmpty(transition.queryParams.cliente)){
-        set(this, "cliente", transition.queryParams.cliente);
+      if (!isEmpty(transition.queryParams.cliente)) {
+        set(this, 'cliente', transition.queryParams.cliente);
       }
-    } catch(err){
-      info("error message en oferta probando", err.message);
+    } catch(err) {
+      info('error message en oferta probando', err.message);
     }
-    info("enbefore model de oferta2");
-    if (origenCliente ){
-      "selectedEtapa selectedManzana selectedInmueble".w().forEach( function(key) {
-        info(key, controller.get(key));
+    info('enbefore model de oferta2');
+    if (origenCliente) {
+      'selectedEtapa selectedManzana selectedInmueble'.w().forEach((key)=> {
+        info(key, get(controller, key));
       });
-      set(this,"noModel", true);
+      set(this, 'noModel', true);
       return;
     }
-    
     controller.setProperties({
-      processingGrabar : false, 
-      selectedEtapa : null,      
-      selectedPrecio : null,
-      selectedManzana : null,
+      processingGrabar: false,
+      selectedEtapa: null,
+      selectedPrecio: null,
+      selectedManzana: null,
       selectedLote: null,
-      selectedInmueble : null,
-      hayClientesSinOfertas : false,
-      afiliacion : '',
-      comision:false,
-      errorMessageComision:"El asesor no tiene comision",
-      cliente : '',
+      selectedInmueble: null,
+      hayClientesSinOfertas: false,
+      afiliacion: '',
+      comision: false,
+      errorMessageComision: 'El asesor no tiene comision',
+      cliente: '',
       numerointerior: '',
       numeroexterior: '',
-      errorMessage:"",
+      errorMessage: '',
       departamento: false,
-      montocredito : '',
-      precio : '',
-      inmueble : '',
+      montocredito: '',
+      precio: '',
+      inmueble: '',
       afiliacionOk: false,
-      afiliacionNoChecar : false,
-      prospectoNoChecar : false,
-      manzana : '',
-      lote : '',
-      apartado : '',
-      prospecto : '',
-      cuantosprecios : 0,
+      afiliacionNoChecar: false,
+      prospectoNoChecar: false,
+      manzana: '',
+      lote: '',
+      apartado: '',
+      prospecto: '',
+      cuantosprecios: 0,
       gastosadministrativos: '',
       precioseguro: '',
       prerecibo: '',
@@ -81,71 +75,57 @@ RouteAuthMixin ,
       subsidio: '',
       pagare: '',
       cuantosInmueblesDisponibles: 0,
-      sumaCheca : false,
-      precioRaw : null,
+      sumaCheca: false,
+      precioRaw: null,
       candadoPrecio: false,
       precioCatalogo: 0
     }); // conforme se definen properties en el controller modificar este hash agregando las properties con el mismo valor asociado
-    
   },
-
-	
-	setupController(ctrlr, model) {
-    info("printicipio de setupController");
-    if(get(this, "cliente")){ 
-      set(ctrlr, "cliente", get(this,"cliente"));
+  setupController(ctrlr, model) {
+    info('printicipio de setupController');
+    if (get(this, 'cliente')) {
+      set(ctrlr, 'cliente', get(this, 'cliente'));
     }
-    
-    if (get(this,"noModel")){ 
+    if (get(this, 'noModel')) {
       return;
     }
-     
-    set(ctrlr,"etapasofertas", model.etapasoferta);
-    const reservados = get(ctrlr,"reservados");
-    info( "reservados ", reservados);
-    if ( reservados > 0 ){
-      set(ctrlr,"flagLista", true);
-      info("flagLista");
+    set(ctrlr, 'etapasofertas', model.etapasoferta);
+    let reservados = get(ctrlr, 'reservados');
+    info('reservados', reservados);
+    if (reservados > 0) {
+      set(ctrlr, 'flagLista', true);
+      info('flagLista');
     }
   },
-
   model() {
-    info("en model");
-    if ( this.get("noModel")){
+    info('en model');
+    if (this.get('noModel')) {
       return [];
     }
-    info("en model2");
-    
-    var store = this.store;
-   
-    return Ember.RSVP.hash({
-     // gtevdor : store.find('zenversion',1),
-      etapasoferta: store.findAll('etapasoferta', {reload: true})
-
+    info('en model2');
+    let { store } = this;
+    return hash({
+      etapasoferta: store.findAll('etapasoferta', { reload: true })
     });
   },
-  
   actions: {
-    error(error, transition){
-      info("paso por aqui error hahaha");
-      //this.transitionTo("index");
+    error(error, transition) {
+      info('error en ruta oferta', error);
     },
-    willTransition(transition){
-      info("en willTransition");
-      var controller = this.controllerFor(this.routeName);
-      var adonde = transition.targetName;
-
-      if ( adonde.indexOf("cliente") !== -1 ){
-      } else {
+    willTransition(transition) {
+      info('en willTransition');
+      let controller = this.controllerFor(this.routeName);
+      let adonde = transition.targetName;
+      if (adonde.indexOf('cliente') === -1) {
         try {
-          controller.send("descartaInmueble");
-          controller.send("descartaProspecto");
+          controller.send('descartaInmueble');
+          controller.send('descartaProspecto');
         } catch (errorson) {
           info(errorson.message);
         }
-      } 
-      get(controller,"inmueblesReservados.content").clear();
-      get(controller,"prospectosReservados.content").clear();
+      }
+      get(controller, 'inmueblesReservados.content').clear();
+      get(controller, 'prospectosReservados.content').clear();
       return true;
     }
   }
