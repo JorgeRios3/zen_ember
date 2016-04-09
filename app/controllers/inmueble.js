05a2ae643c0fcaa157aa0688a7196b5bd56c2dd0
Jorge Rios <jrios@grupoiclar.com>
2016-04-08 19:29:03 -0500

    haciendo suave de archivos

--- app/controllers/inmueble.js
+++ app/controllers/inmueble.js
@@ -2,43 +2,38 @@ import Ember from 'ember';
 
 
 const {
-	get,
-	set,
-	observer,
-	computed,
-	isEmpty
+  get,
+  set,
+  observer,
+  computed,
+  isEmpty,
+  Logger: { info }
 } = Ember;
 
-var info=Ember.Logger.info;
 export default Ember.Controller.extend({
-	session:Ember.inject.service(),
-	disponible:false,
-	selectedEtapa:"",
-	listaInmuebles:"",
-	desplegarResultado:false,
+  session:Ember.inject.service(),
+  disponible:false,
+  selectedEtapa:'',
+  listaInmuebles:'',
+  desplegarResultado:false,
+  buscarActivado:computed('selectedEtapa',{
+    get(){
+      return parseInt(get(this, 'selectedEtapa'))>0;
+    }
+  }),
 
-	buscarActivado:computed("selectedEtapa",{
-		get(){
-			return parseInt(get(this, "selectedEtapa"))>0;
-		}
-	}),
-
-
-	actions:{
-		selectedEtapaAction(item){
-			
-			set(this, "selectedEtapa", get(item, "id"));
-		},
-		buscar(){
-			set(this, "desplegarResultado", true);
-			var that=this;
-			var hash={etapa:get(this, "selectedEtapa")};
-			if(get(this, "disponible")){
-				hash.disponible=1;
-			}
-			set(this, "listaInmuebles",this.store.query("inmuebledetalle", hash));
-
-
-		}
+  actions: {
+    selectedEtapaAction(item) {	
+	  set(this, 'selectedEtapa', get(item, 'id'));
+    },
+    buscar() {
+      set(this, 'desplegarResultado', true);
+      let that=this;
+      var hash={ etapa: get(this, 'selectedEtapa') };
+      if (get(this, 'disponible')) {
+        hash.disponible=1;
+      }
+	  set(this, 'listaInmuebles',this.store.query('inmuebledetalle', hash));
 	}
+  }
 });
