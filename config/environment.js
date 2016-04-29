7d60029e140799a7f210f071645a4fd6079c185a
Jorge Rios <jrios@grupoiclar.com>
2016-04-29 10:53:32 -0500

    se cambia index.html y environment.js con rootURL

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'zeniclar',
     environment: environment,
-    baseURL: '/zeniclar/',
+    rootURL: '/zeniclar/',
     locationType: 'hash',
     contentSecurityPolicyHeader: 'Content-Security-Policy',
     EmberENV: {
