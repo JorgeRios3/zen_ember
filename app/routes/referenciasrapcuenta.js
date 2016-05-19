c49cb5e692cd4e4b0cbadbee32060aa000304925
Jorge Rios <jrios@grupoiclar.com>
2016-05-19 13:35:23 -0500

    se pule referenciasrapcuenta

--- app/routes/referenciasrapcuenta.js
+++ app/routes/referenciasrapcuenta.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
-
-export default Ember.Route.extend({
+import RouteAuthMixin from '../mixins/routeauth';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+export default Ember.Route.extend(AuthenticatedRouteMixin, RouteAuthMixin, {
+  beforeModel() {
+  	this._super(...arguments);
+    let controller = this.controllerFor(this.routeName);
+    controller.setProperties({
+      referenciaRap: '',
+      nombre: '',
+      cuenta: '',
+      referencia: '',
+      cliente: '',
+      etapa: '',
+      manzana: '',
+      lote: '',
+      saldo: '',
+      lista: Ember.A(),
+      clientesLista: null
+    });
+  }
 });
