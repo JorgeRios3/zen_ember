22d6cabbdac84991155bb8a175a304931360555e
Jorge Rios <jrios@grupoiclar.com>
2016-05-26 09:56:49 -0500

    se pule estadodecuenta no tenia etapa

--- app/models/cuentabreve.js
+++ app/models/cuentabreve.js
@@ -4,6 +4,7 @@ export default DS.Model.extend({
   nombre: DS.attr('string'),
   manzana: DS.attr('string'),
   lote: DS.attr('string'),
-  inmueble: DS.attr('string')
+  inmueble: DS.attr('string'),
+  etapa: DS.attr('string')
   
 });
