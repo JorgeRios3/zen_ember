c4f3ab9de8ac521bb2f6bebf0627009459a5f1de
Jorge Rios <jrios@grupoiclar.com>
2016-06-15 09:35:28 -0500

    se agrega a ruta arcadiainmuebles el routeath para autentificacion

--- app/routes/arcadiainmuebles.js
+++ app/routes/arcadiainmuebles.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
+import RouteAuthMixin from '../mixins/routeauth';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
+
+export default Ember.Route.extend(AuthenticatedRouteMixin, RouteAuthMixin,{
+  init() {
+    this._super(...arguments);
+  }
 });
