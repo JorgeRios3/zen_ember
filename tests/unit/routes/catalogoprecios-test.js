ce5eb0f5eb6a9705b3662608bd57f6f9018c4815
Jorge Rios <jrios@grupoiclar.com>
2017-01-16 12:50:31 -0600

    lista oferta pelando el descuentoprecio en la suma de cantidades

---
+++ tests/unit/routes/catalogoprecios-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:catalogoprecios', 'Unit | Route | catalogoprecios', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
