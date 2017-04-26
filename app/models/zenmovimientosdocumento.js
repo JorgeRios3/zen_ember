dea98caf0120d24dfbb70df42c12397bea77a828
Jorge Rios <jrios@grupoiclar.com>
2017-04-26 13:06:43 -0500

    cancelacion de descuentos en estado de cuenta iclar

--- app/models/zenmovimientosdocumento.js
+++ app/models/zenmovimientosdocumento.js
@@ -1,9 +1,21 @@
 import DS from 'ember-data';
+const {
+computed,
+get,
+set, 
+Logger: {info},
+observer
+} = Ember;
 
 export default DS.Model.extend({
   cantidad:DS.attr("string"),
   fecha:DS.attr("string"),
   cargoabono:DS.attr("string"),
   recibo:DS.attr("number"),
-  relaciondepago: DS.attr('string', {default: ""})
+  relaciondepago: DS.attr('string', {default: ""}),
+  esdescuento: computed('relaciondepago', {
+  	get() {
+  	  return get(this, 'relaciondepago') === 'DESCUENTO PRECIO';
+  	}
+  })
 });
